67c02b90aaf18ae23a26ab1684f9271bdde60e52
Prince3877 <bernardwambua43@gmail.com>
2022-10-17 23:59:49 -0700

    C hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Return 0 (Success)
- */
-
+/*
+ * main - print a line of codes using puts
+ * Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
